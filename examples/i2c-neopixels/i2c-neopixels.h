67d64965c61cc2f83cb9b808c307fe7352a38b66
Philip Howard <phil@gadgetoid.com>
2013-11-03 21:52:23 +0000

    Updated neopixel example

--- examples/i2c-neopixels/i2c-neopixels.h
+++ examples/i2c-neopixels/i2c-neopixels.h
@@ -1,12 +1,35 @@
+/************************
+* VALUES YOU CAN CHANGE *
+*************************/
+
+// The I2C address that your Trinket will use
+#define I2C_SLAVE_ADDRESS 0x4
+
+// NeoPixel PIN, and pixel count
+// Brightness MUST be 255 or colours can't be read back correctly from the strip
+#define PIN 1 // Pin the strip is connected to
+#define PIXELS 7 // Maximum of 85 pixels
+#define PIXEL_COLOR_MODE NEO_GRB // You might want to change this to NEO_RGB if you're using anything but real NeoPixels
+
+/************************
+*    EVERYTHING ELSE    *
+*************************/
+
+// Only change this if you have modified in TinyWireS
+#ifndef TWI_RX_BUFFER_SIZE
+#define TWI_RX_BUFFER_SIZE ( 16 )
+#endif
+
 #define __AVR_ATtiny85_
 
 // These define the commands available when writing via i2c
-#define CMD_SET_PIXEL_COLOR_RGB 0x01
-// i2cset -y 1 0x04 0x01 0x00 0xFF 0xFF 0xFF 0xFF 0x00 0x00   - Set first pixel to white and second pixel to red
-#define CMD_SET_PIXEL_COLOR_WHEEL 0x02
+#define CMD_ALL_OFF 0x00
+#define CMD_ALL_RGB 0x01
+#define CMD_SOME_RGB 0x02
 
-int32_t Wheel(byte WheelPos);
-void requestEvent();
-void receiveEvent(uint8_t NumBytes);
+void updatePixelColor(uint8_t value);
+void handleRead();
+void handleWrite(uint8_t NumBytes);
+void handleCommand(uint8_t NumBytes);
 void setup();
 void loop();
--- examples/i2c-neopixels/i2c-neopixels.ino
+++ examples/i2c-neopixels/i2c-neopixels.ino
@@ -1,146 +1,149 @@
 #include <TinyWireS.h>
 #include <Adafruit_NeoPixel.h>
+
+// Open this header file to configure the NeoPixel i2c driver
 #include "i2c-neopixels.h"
 
-// The I2C address that your Trinket will use
-#define I2C_SLAVE_ADDRESS 0x4
+// Neo KHZ400 is necessary on the 8Mhz Trinket
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN, PIXEL_COLOR_MODE + NEO_KHZ400);
 
-// NeoPixel PIN, pixel count and brightness
-#define PIN 1
-#define PIXELS 7 // My NeoPixel stick is broken!
-#define BRIGHTNESS 200
+// Tracks the current register pointer position and current pixel
+// There are 3x as many "registers" as pixels, RGB*PIXELS
+volatile uint8_t current_register = 0;
+volatile uint8_t current_command = 0;
+volatile uint8_t current_pixel = 0;
 
-// You might want to change this to NEO_RGB if you're using anything but real NeoPixels
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN, NEO_GRB + NEO_KHZ400);
+uint8_t r, g, b, x, n, o;
 
-// Registers to store the colours of each pixel
-// volatile uint8_t i2c_regs[PIXELS];
-
-// Tracks the current register pointer position
-volatile byte reg_position;
-volatile uint16_t current_pixel;
-
-// Function to convert int to rgb colour, from AdaFruit example
-int32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
-
-/**
- * This is called for each read request we receive, never put more than one byte of data (with TinyWireS.send) to the 
- * send-buffer when using this callback
- */
-void requestEvent()
-{  
-    // Implements i2cget, we'll implement this later
-    // TinyWireS.send(i2c_regs[reg_position]);
-    // Increment the reg position on each read, and loop back to zero
-    //reg_position = (reg_position+1) % sizeof(i2c_regs);
-}
-
-/**
- * The I2C data received -handler
- *
- * This needs to complete before the next incoming transaction (start, data, restart/stop) on the bus does
- * so be quick, set flags for long running tasks to be called from the mainloop instead of running them directly,
- */
-void receiveEvent(uint8_t NumBytes)
+// Updates an individual color component according to the register position
+void updatePixelColor(uint8_t value)
 {
-    if (NumBytes < 1)
-    {
-        // No data
-        return;
-    }
+	current_pixel = (uint8_t)floor(current_register / 3) % (PIXELS*3);
 
-    if (NumBytes > TWI_RX_BUFFER_SIZE)
-    {
-        // Also insane number
-        return;
-    }
+	r = strip.getPixelColor( current_pixel ) >> 16;
+	g = strip.getPixelColor( current_pixel ) >> 8;
+	b = strip.getPixelColor( current_pixel );
 
-    uint8_t command = TinyWireS.receive();
+	switch(current_register % 3){
+		case 0: // red
+			strip.setPixelColor( current_pixel, value, g, b );
+			break;
+		case 1: // blue
+			strip.setPixelColor( current_pixel, r, value, b );
+			break;
+		case 2: // green
+			strip.setPixelColor( current_pixel, r, g, value );
+			break;
+	}
+}
+
+// Handles each Read request
+void handleRead()
+{  
+	// Each pixel has 3 components, R, G and B, to get the pixel index divide by 3
+	current_pixel = floor(current_register / 3);
+
+	uint8_t component_color = (uint8_t)( strip.getPixelColor( current_pixel ) >> (16 - ((current_register % 3)*8)) );
+
+	TinyWireS.send(component_color);
+
+	current_register = (current_register+1) % (PIXELS*3);
+}
+
+// Handles each Write request
+void handleWrite(uint8_t NumBytes)
+{
+	// Exit if there is no data or more data than the buffer should ever allow
+    if (NumBytes < 1 || NumBytes > TWI_RX_BUFFER_SIZE) return;
+
+	// Always treat the first byte as the address
+    current_register = TinyWireS.receive();
     NumBytes--;
 
-    switch(command){
-      case CMD_SET_PIXEL_COLOR_RGB:
-           
-        current_pixel = TinyWireS.receive();
+	if( current_register == 255 && NumBytes )
+	{
+		handleCommand(NumBytes);
+	}
+	else
+	{
+		// If one byte, then addr set for next read
+		if (!NumBytes) return;
 
-        NumBytes--;
+		// Loop through the remaining bytes and update the pixel components sequentially
+		while (NumBytes--)
+		{
+			updatePixelColor(TinyWireS.receive());
+			current_register++;
+		}
+	}
+	
+	// Update the strip
+    strip.show();
+}
 
-        if (!NumBytes || NumBytes%3)
-        {
-          // Either no bytes, or not a valid multiple of 3 ( RGB RGB RGB etc )
-          return;
-        }      
+void handleCommand(uint8_t NumBytes)
+{
+	current_command = TinyWireS.receive();
+	NumBytes--;
+	switch(current_command){
+		case CMD_ALL_OFF:
+			for(x=0;x<PIXELS;x++){
+				strip.setPixelColor(x,0,0,0);
+			}
+			break;
+		case CMD_ALL_RGB:
+			if (NumBytes < 3) return; // Not got R G and B components
+			r = TinyWireS.receive();
+			g = TinyWireS.receive();
+			b = TinyWireS.receive();
+    			NumBytes-=3;
 
-        while(NumBytes>0)
-        {
-          // Receive R, G and B values
-          strip.setPixelColor(current_pixel,TinyWireS.receive(),TinyWireS.receive(),TinyWireS.receive());
-          current_pixel++; // De-increment by 3 ( RGB )
-          NumBytes-=3;
-        }
+			for(x=0;x<PIXELS;x++){
+				strip.setPixelColor(x,r,g,b);
+			}
+			break;
+		case CMD_SOME_RGB:
+			if (NumBytes < 4) return; // Not got R G and B components
+			r = TinyWireS.receive();
+			g = TinyWireS.receive();
+			b = TinyWireS.receive();
+			n = TinyWireS.receive();
+    			NumBytes-=4;
+	
+			o = 0; // Starting pixel
 
-        strip.show();
+			if(NumBytes)
+			{
+				o = TinyWireS.receive();
+				NumBytes--;
+			}
 
-      break;
-      case CMD_SET_PIXEL_COLOR_WHEEL:
-        
-        current_pixel = TinyWireS.receive();
-
-        NumBytes--; // Start pixel has been read
-
-        if (!NumBytes)
-        {
-          // No bytes for pixel colours
-          return;
-        }
-        while(NumBytes--)
-        {
-            // Update NeoPixels
-            uint8_t col = TinyWireS.receive();
-
-            if( col > 0 ){
-              strip.setPixelColor(current_pixel%PIXELS, Wheel(col));
-            }
-            else
-            {
-              strip.setPixelColor(current_pixel%PIXELS, 0, 0, 0); // Set pixel off
-            }
-
-            // Increment register position
-            current_pixel++;
-        }
-        strip.show();
-        break;
-      }
+			for(x=0;x<PIXELS-o;x++){
+				if(x%n == 0){
+					strip.setPixelColor(x+o,r,g,b);
+				}
+			}
+			break;
+	}
+	// Consume any leftover bytes
+	while (NumBytes--)
+	{
+		TinyWireS.receive();
+	}
 }
 
 void setup()
 {
     TinyWireS.begin(I2C_SLAVE_ADDRESS);
-    TinyWireS.onReceive(receiveEvent);
-    TinyWireS.onRequest(requestEvent);
+    TinyWireS.onReceive(handleWrite);
+    TinyWireS.onRequest(handleRead);
 
     strip.begin();
     strip.show();
-    strip.setBrightness(BRIGHTNESS);
 }
 
 void loop()
 {
-    /**
-     * This is the only way we can detect stop condition (http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&p=984716&sid=82e9dc7299a8243b86cf7969dd41b5b5#984716)
-     * it needs to be called in a very tight loop in order not to miss any (REMINDER: Do *not* use delay() anywhere, use tws_delay() instead).
-     * It will call the function registered via TinyWireS.onReceive(); if there is data in the buffer on stop.
-     */
+    // Avoid putting anything in this loop, and *DO NOT* use delay() anywhere
     TinyWireS_stop_check();
 }
